8d8685598b495c1166d0ed4f32b6fb14386cb5f5
Oguz <gozlu.oguz@gmail.com>
2024-02-05 00:24:57 +0300

    implement async main and balance update at start

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,78 @@
 import * as commander from 'commander';
+import clc from 'cli-color';
 import {
   createWallet,
   selectWallet,
   performAirdrop,
   checkWalletBalance,
   loadWallet,
+  transfer,
+  updateBalances
 } from './wallet'; // Adjust the path accordingly
 
-const program = new commander.Command();
-program.version('1.0.0');
+import { getNetworkStatus } from "./utils"
 
-program
-  .command('new [walletName]')
-  .description('Initialize a new wallet with an optional name')
-  .action((walletName) => {
-    createWallet(walletName);
-  });
+async function main() {
 
-program
-  .command('select <walletName>')
-  .description('Select a wallet for default operations')
-  .action((walletName) => {
-    selectWallet(walletName);
-  });
-
-program
-  .command('airdrop [X]')
-  .description('Airdrop X SOL to the selected wallet (default 1)')
-  .option('-l, --left', 'Use "LEFT" as the airdropped SOL amount')
-  .action((X, options) => {
-    performAirdrop(X, options);
-  });
-
-program
-  .command('balance')
-  .description('Check balance of the selected wallet')
-  .action(() => {
-    checkWalletBalance();
-  });
-
-program.parse(process.argv);
-
-// If no command is specified, show help
-if (!process.argv.slice(2).length) {
   const walletData = loadWallet();
   const selectedWalletName = walletData.selectedWallet || 'No wallet selected';
-  program.outputHelp((help) => help.replace('Usage:', `Selected Wallet: ${selectedWalletName}\n\nUsage:`));
+  console.log(clc.magentaBright("Selected Wallet: ") + clc.cyanBright(selectedWalletName))
+
+  await updateBalances();
+
+  const program = new commander.Command();
+  program.version('1.0.0');
+
+  program
+    .command('new [walletName]')
+    .description('Initialize a new wallet with an optional name')
+    .action((walletName) => {
+      createWallet(walletName);
+    });
+
+  program
+    .command('select <walletName>')
+    .description('Select a wallet for default operations')
+    .action((walletName) => {
+      selectWallet(walletName);
+    });
+
+  program
+    .command('airdrop [X]')
+    .description('Airdrop X SOL to the selected wallet (default 1)')
+    .action((X) => {
+      performAirdrop(X);
+    });
+
+  program
+    .command('balance')
+    .description('Check balance of the selected wallet')
+    .action(() => {
+      checkWalletBalance();
+    });
+
+  program
+    .command('status')
+    .description('Display Solana network status')
+    .action(() => {
+      getNetworkStatus();
+    });
+
+  program
+    .command('transfer <otherPublicKey> <amount>')
+    .description('Transfer SOL to another wallet')
+    .action((otherPublicKey, amount) => {
+      transfer(otherPublicKey, amount);
+    });
+
+  program.parse(process.argv);
+
+  // If no command is specified, show help
+  if (!process.argv.slice(2).length) {
+    const walletData = loadWallet();
+    const selectedWalletName = walletData.selectedWallet || 'No wallet selected';
+    program.outputHelp((help) => help.replace('Usage:', `Selected Wallet: ${selectedWalletName}\n\nUsage:`));
+  }
 }
 
-// Add any additional logic or commands as needed
+main()
