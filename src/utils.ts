b271d9743c8e0e96ec294666505d1eb86850c319
Oguz <gozlu.oguz@gmail.com>
2024-02-05 16:54:59 +0300

    refactor: move methods to utils.ts from wallet.ts feat: add banner display

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { Connection } from '@solana/web3.js';
 import clc from 'cli-color';
 
+import { loadWallet, saveWallet } from "./wallet"
 const DEVNET_URL = 'https://api.devnet.solana.com';
 
 export async function getNetworkStatus(): Promise<void> {
@@ -17,7 +18,82 @@ export async function getNetworkStatus(): Promise<void> {
         console.log(clc.greenBright(`Block Height: ${blockHeight}`));
         console.log(clc.magentaBright(`Current Slot: ${slot}`));
         console.log(clc.cyanBright(`Transaction Count: ${transactionCount}`));
+
     } catch (error) {
         console.error(clc.red('Error fetching network status:'), error);
     }
+}
+
+export async function updateBalances(): Promise<void> {
+    const walletData = loadWallet();
+    const connection = new Connection(DEVNET_URL);
+
+    let n = 0
+    for (const walletName in walletData.wallets) {
+        const wallet = walletData.wallets[walletName];
+
+        try {
+            const balance = await connection.getBalance(wallet.publicKey);
+            if (wallet.balance != balance) {
+                n++;
+
+                const diff = balance - wallet.balance
+                if (diff > 0) {
+                    // Wallet recieved SOL
+                    console.log(clc.green("Wallet ", walletName, "balance increased by", diff / 1e9, "SOL"))
+
+                } else {
+                    // Wallet lost SOL
+                    console.log(clc.red("Wallet ", walletName, "balance decreased by", diff / 1e9, "SOL"))
+
+                }
+            }
+
+            wallet.balance = balance;
+
+        } catch (error) {
+            console.error(clc.red(`Error updating balance for ${walletName}:`, error));
+        }
+    }
+
+    saveWallet(walletData);
+    if (n != 0) {
+        console.log(clc.italic.bgWhite("Updated", n, "Wallet Balances\n\n"))
+    }
+}
+
+
+export function listWallets(displayPublic: boolean): void {
+    const walletData = loadWallet();
+    const wallets = Object.entries(walletData.wallets);
+
+    if (wallets.length === 0) {
+        console.log(clc.yellow('No wallets found.'));
+        return;
+    }
+
+    console.log(clc.bold.underline.cyanBright('Wallets List:'));
+
+    wallets.forEach(([walletName, wallet], index) => {
+        console.log(
+            clc.magentaBright(`${index + 1}. ${walletName}:`),
+            displayPublic ? clc.cyanBright(wallet.publicKey.toBase58()) : '',
+            clc.greenBright(`Balance: ${wallet.balance / 1e9} SOL`)
+        );
+    });
+}
+
+export function getBanner(): string {
+    return `⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Solana Wallet⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⡀⠀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣆⠀⠀⠀⠀⠀
+    ⠀⠀⢸⣤⡀⠉⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀⠀
+    ⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀
+    ⠀⠀⢸⣿⣿⣿⣿⣿⡿⠛⠛⠛⠛⠛⠛⠛⠛⠛⢛⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+    ⠀⠀⢸⣿⣿⣿⣿⣋⣀⣀⣀⣀⣀⣀⣀⣀⣀⣴⣿⣿⣿⡿⠟⠛⠛⠛⠛⠃⠀⠀
+    ⠀⠀⢸⣿⣿⣿⣟⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⢿⣿⣿⡏⢀⣴⣶⣶⣶⣶⡆⠀⠀
+    ⠀⠀⢸⣿⣿⣿⣿⣷⣦⣀⣀⣀⣀⣀⣀⣀⣀⣀⣙⣿⣇⠈⠻⠿⠿⠿⠿⠇⠀⠀
+    ⠀⠀⢸⣿⣿⣿⣿⣿⡿⠛⠛⠛⠛⠛⠛⠛⠛⠛⢛⣿⣿⣷⣦⣤⣤⣤⣤⠀⠀⠀
+    ⠀⠀⢸⣿⣿⣿⣿⣋⣀⣀⣀⣀⣀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀
+    ⠀⠀⠀⠈⠻⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠁⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀-by odd509⠀⠀⠀`
 }
